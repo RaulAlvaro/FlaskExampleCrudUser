b8a22b784d5b06de09665b58d55f2fd527340c8e
Raul <raulalvaro123@hotmail.com>
2018-11-20 16:39:06 -0500

    Probando CORS

--- users.py
+++ users.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, fields, marshal_with
 from datetime import datetime
 import os
+from flask_cors import CORS
 
 dbdir = "sqlite:///" + os.path.abspath(os.getcwd()) + "/database.db"
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = dbdir
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
